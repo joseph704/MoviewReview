5c347b3bb9fbe9c2e47b0944c9d26b334e398903
차요셉 <jscha@raoncorp.com>
2022-08-02 21:01:27 +0900

    SwiftLint 추가

---
+++ MovieReview/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieReview
+//
+//  Created by Joseph Cha on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
